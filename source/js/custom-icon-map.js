8b6c3716be4d2e47697d9a33b6eedf30db99ca01
CendresChaudes <romqaaa1337@gmail.com>
2022-12-19 23:06:41 +0300

    Добавляет кастомную иконку на карту

---
+++ source/js/custom-icon-map.js
@@ -0,0 +1,31 @@
+ymaps.ready(function () {
+  let myMap = new ymaps.Map('map', {
+          center: [59.938635, 30.323118],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Мишка',
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '../img/map-pin.svg',
+          // Размеры метки.
+          iconImageSize: [67, 100],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-33, -100]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark)
+});
