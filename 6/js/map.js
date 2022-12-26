116a05467f264869c6272b0f7e0dd0d577b33946
Keks <keks@htmlacademy.ru>
2022-12-26 23:35:51 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/map.js
@@ -0,0 +1,36 @@
+let imageMap = document.querySelector(".contacts__map");
+
+imageMap.classList.remove("contacts__map--nojs");
+
+ymaps.ready(function () {
+  let myMap = new ymaps.Map('map', {
+          center: [59.938635, 30.323118],
+          zoom: 17,
+          controls: []
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Мишка',
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '../img/svg/map-pin.svg',
+          // Размеры метки.
+          iconImageSize: [67, 100],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-33, -100]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark)
+});
